a8f6af961671846dc76f053d136c18c7e61d9c85
eman-m0hamed <emanmohamed73359@gmail.com>
2024-03-06 18:41:06 +0200

    create add ip blade

--- resources/views/restrict_ip/create.blade.php
+++ resources/views/restrict_ip/create.blade.php
@@ -1,9 +1,38 @@
-{{ Form::open(['route' => ['create.ip'], 'method' => 'post']) }}
+@if (Session::has('success'))
+<div class="alert alert-success">{{ Session::get('success') }}</div>
+@endif
+
+@if (Session::has('error'))
+<div class="alert alert-danger">{{ Session::get('error') }}</div>
+@endif
+
+
+{{ Form::open(['route' => ['store.ip'], 'method' => 'post']) }}
 <div class="modal-body">
     <div class="row">
         <div class="form-group">
+            @error('ip')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
             {{ Form::label('ip', __('IP'), ['class' => 'col-form-label']) }}
             {{ Form::text('ip', null, ['class' => 'form-control', 'placeholder' => 'Enter Ip Address']) }}
+        </div>
+        <div class="form-group">
+            @error('belongs_to')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+            {{ Form::label('belongs_to', __('belongs_to'), ['class' => 'col-form-label']) }}
+
+           <select name="belongs_to" id="belongs_to">
+            <option value="0" selected disabled>Select The owner of this Ip</option>
+
+            @foreach ($users as $user )
+
+            <option value="{{$user->id}}">{{$user->name}}</option>
+
+            @endforeach
+
+           </select>
         </div>
     </div>
 </div>
