f77f4da4d90e574881b9759c303a5f26c3b795c1
eman-m0hamed <emanmohamed73359@gmail.com>
2024-03-14 15:02:34 +0200

    handle manager dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -40,7 +40,7 @@ class HomeController extends Controller
 
         if (Auth::check()) {
             $user = Auth::user();
-            if ($user->type == 'employee') {
+            if ($user->type == 'employee' || $user->type == 'manager') {
                 $emp = Employee::where('user_id', '=', $user->id)->first();
 
                 $announcements = Announcement::orderBy('announcements.id', 'desc')->take(5)->leftjoin('announcement_employees', 'announcements.id', '=', 'announcement_employees.announcement_id')->where('announcement_employees.employee_id', '=', $emp->id)->orWhere(
