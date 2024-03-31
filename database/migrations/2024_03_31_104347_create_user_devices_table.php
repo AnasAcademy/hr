5ff7c79c4940cf05281237701b24135dc8c1b1dd
eman-m0hamed <emanmohamed73359@gmail.com>
2024-03-31 14:58:12 +0200

    edit user devices migration file

--- database/migrations/2024_03_31_104347_create_user_devices_table.php
+++ database/migrations/2024_03_31_104347_create_user_devices_table.php
@@ -15,7 +15,12 @@ return new class extends Migration
         Schema::create('user_devices', function (Blueprint $table) {
             $table->id();
             $table->string('ip');
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 10, 8);
             $table->string('date');
+            $table->string('os_name');
+            $table->string('device_type');
+            $table->string('browser_name');
             $table->json('Details');
             $table->foreignId('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
             $table->foreignId('created_by')->nullable()->references('id')->on('users')->nullOnDelete()->cascadeOnUpdate();
